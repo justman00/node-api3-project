a1f9a2c8b75eb786ac4d43d6be9b97bf3978fbab
Ekaterina173 <ecaterina.popa18@gmail.com>
2021-03-02 11:56:07 +0200

    deploy2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // require your server and launch it
 const server = require("./api/server");
 
-const port = process.env.REACT_APP_PORT ||  4000;
-console.log(process.env.REACT_APP_STEP_IT)
+const port = process.env.PORT ||  4000;
+console.log(process.env.STEP_IT)
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
