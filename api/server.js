b60589a1af17be3289b50234b90eb17c0ca4fdb4
Ekaterina173 <51832550+Ekaterina173@users.noreply.github.com>
2021-03-06 10:25:50 +0200

    remove mongo

--- api/server.js
+++ api/server.js
@@ -3,20 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 console.log(process.env.MONGO_DB_USERNAME, process.env.MONGO_DB_PASSWORD);
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@stepit-cluster.v8xqd.mongodb.net/Blog?retryWrites=true&w=majority`
-);
+// mongoose.connect(
+//   `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@stepit-cluster.v8xqd.mongodb.net/Blog?retryWrites=true&w=majority`
+// );
 
 const logger = require("./middleware/loggerMiddleware");
 const error = require("./middleware/errorMiddleware");
-//const welcomeRouter = require("./welcome/welcome-router");
+const welcomeRouter = require("./welcome/welcome-router");
 const usersRouter = require("./users/users-router");
 const postsRouter = require("./posts/posts-router");
 const server = express();
 
 server.use(logger("combined"));
 server.use(express.json());
-//server.use(welcomeRouter);
+server.use(welcomeRouter);
 server.use("/api/users", usersRouter);
 server.use("/api/posts", postsRouter);
 server.use(error);
