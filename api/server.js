dcb2121249811c01d26aa6b77321c4692ffe271f
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-03-06 14:22:52 +0200

    add another endpoint for /

--- api/server.js
+++ api/server.js
@@ -35,4 +35,8 @@ server.use(logger);
 server.use('/api/users', usersRouter);
 server.use('/api/posts', postsRouter);
 
+server.get('/', (req, res) => {
+  res.send(`<h1>Welcome to the API</h1>`);
+});
+
 module.exports = server;
