272a2e0c16d52ec8b3b9f1c2da345405b7895747
Ghenadie <hilotiighenadie0@gmail.com>
2021-03-06 11:46:00 +0000

    abc

--- api/server.js
+++ api/server.js
@@ -1,21 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.7j4py.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 
-const {logger} = require('./middleware/middleware');
 
-const usersRouter = require('./users/users-router');
-const postsRouter = require('./posts/posts-router');
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.7j4py.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("MongoDB connected!!");
+  } catch (err) {
+    console.log("Failed to connect to MongoDB", err);
+  }
+};
+connectDB();
+
+const { logger } = require("./middleware/middleware");
+
+const usersRouter = require("./users/users-router");
+const postsRouter = require("./posts/posts-router");
 
 const server = express();
 server.use(cors());
 
 server.use(logger);
-server.use('/api/users', usersRouter);
-server.use('/api/posts', postsRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
 
 module.exports = server;
